8c578982616ce6902f448c4ab22f428e9f787517
KD Circuits <kevin@kdcircuits.com>
2022-02-09 19:21:17 -0500

    update to toc

--- conf.py
+++ conf.py
@@ -77,14 +77,14 @@ pygments_style = None
 html_theme = "sphinx_rtd_theme"
 
 
-
-
-
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+#html_theme_options = {
+#'collapse_navigation': True,
+#'navigation_depth': -1
+# }
 #html_theme_options = {
 #    'canonical_url': '',
 #    'analytics_id': 'UA-XXXXXXX-1',  #  Provided by Google in your dashboard
@@ -101,6 +101,9 @@ html_theme = "sphinx_rtd_theme"
 #    'includehidden': True,
 #    'titles_only': False
 #}
+
+
+
 
 #html_theme_options = {
 #    'logo_only': true,
